0b4f38373b0b4cd03cafc785b2b52b6f068fb914
RajrupDasid <rajrupdasid@outlook.com>
2022-08-28 21:22:14 +0530

    asgi file updated for async file

---
+++ backend/asgi.py
@@ -0,0 +1,21 @@
+"""
+ASGI config for backend project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/4.1/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+from channels.routing import ProtocolTypeRouter
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'backend.settings')
+# Initialize Django ASGI application early to ensure the AppRegistry
+# is populated before importing code that may import ORM models.
+django_asgi_app = get_asgi_application()
+application = ProtocolTypeRouter({
+    "http": django_asgi_app,
+})
+#application = get_asgi_application()
