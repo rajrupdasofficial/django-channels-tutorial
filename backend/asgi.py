e8e8851b93f16d2e96904f5b9d81d12b1b371901
RajrupDasid <rajrupdasid@outlook.com>
2022-08-30 09:24:59 +0530

    asgi core functionalities has been changed

--- backend/asgi.py
+++ backend/asgi.py
@@ -10,12 +10,23 @@ https://docs.djangoproject.com/en/4.1/howto/deployment/asgi/
 import os
 
 from django.core.asgi import get_asgi_application
-from channels.routing import ProtocolTypeRouter
+from channels.routing import ProtocolTypeRouter,URLRouter
+from channels.security.websocket import AllowedHostsOriginValidator
+from channels.auth import AuthMiddlewareStack
+import chat.routing
+
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'backend.settings')
 # Initialize Django ASGI application early to ensure the AppRegistry
 # is populated before importing code that may import ORM models.
 django_asgi_app = get_asgi_application()
 application = ProtocolTypeRouter({
     "http": django_asgi_app,
+    "websocket":AllowedHostsOriginValidator(
+        AuthMiddlewareStack(
+            URLRouter(
+                chat.routing.websocket_urlpatterns
+            )
+        )
+    ),
 })
 #application = get_asgi_application()
