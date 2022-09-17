5697783145eb0e3665ec165e8657e2ea24335c35
RajrupDasid <rajrupdasid@outlook.com>
2022-09-17 11:20:26 +0530

    views logic added

--- chat/views.py
+++ chat/views.py
@@ -1,8 +1,19 @@
+import imp
+from tokenize import group
+from unicodedata import name
 from django.shortcuts import render
-
+from .models import Chat,Group
 # Create your views here.
 
 def index(request):
+    room_name=Group.objects.filter(name=name).first()
+  
+    if room_name:
+        pass
+    else:
+        room_name=Group(name=name)
+    room_name.save()
+    
     return render(request,'chat/index.html')
 
 def room(request,room_name):
