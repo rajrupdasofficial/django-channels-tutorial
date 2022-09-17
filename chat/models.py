d5c543d6ea335d405f2e4410a2b3980ab54717c0
RajrupDasid <rajrupdasid@outlook.com>
2022-09-17 11:20:03 +0530

    models logic added

--- chat/models.py
+++ chat/models.py
@@ -1,3 +1,13 @@
+from tokenize import group
 from django.db import models
 
 # Create your models here.
+
+class Group(models.Model):
+    name = models.CharField(max_length=155)
+class Chat(models.Model):
+    content=models.CharField(max_length=10000)
+    timestamp=models.DateTimeField(auto_now=True)
+    group=models.ForeignKey('Group',on_delete=models.CASCADE)
+
+
